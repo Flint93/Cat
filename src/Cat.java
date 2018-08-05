54ba77ccdd66a99f3a793fd85dd8847b823949e7
Flint93 <41943875+Flint93@users.noreply.github.com>
2018-08-05 16:46:56 +0300

    Initial commit

---
+++ src/Cat.java
@@ -0,0 +1,117 @@
+
+public class Cat
+{
+    private Double originWeight;
+    private Double weight;
+    private Double minWeight;
+    private Double maxWeight;
+    private Double howMuchFood;
+    private Boolean alive;
+    private static Integer count = 0;
+
+    public boolean getStatusAlive()
+    {
+        return alive;
+    }
+
+    public Cat()
+    {
+            weight = 1500.0 + 3000.0 * Math.random();
+            originWeight = this.weight;
+            minWeight = 1000.0;
+            maxWeight = 9000.0;
+            howMuchFood = 0.0;
+            alive = true;
+            count++;
+    }
+
+    Cat(Double weight)
+    {
+        this();
+        if (this.weight < 1500 || this.weight > 4500)
+        {
+            System.out.println("Неверно указан вес кошки, он должен быть от 1500 г до 4500 г");
+        }
+        else
+        {
+            this.weight = weight;
+        }
+
+    }
+
+    public void meow()
+    {
+        weight = weight - 1;
+        System.out.println("Meow");
+        getStatus();
+    }
+
+    public void feed(Double amount)
+    {
+        weight = weight + amount;
+        howMuchFood += amount;
+        getStatus();
+    }
+
+    public void drink(Double amount)
+    {
+        weight = weight + amount;
+        getStatus();
+    }
+
+    public Double getWeight()
+    {
+        return weight;
+    }
+
+    public String getStatus()
+    {
+        if(weight < minWeight)
+        {
+            if (this.alive)
+            {
+                count--;
+                this.alive = false;
+            }
+            return "Dead";
+        }
+        else if(weight > maxWeight)
+        {
+            if (this.alive)
+            {
+                count--;
+                this.alive = false;
+            }
+            return "Exploded";
+        }
+        else if(weight > originWeight)
+        {
+            return "Sleeping";
+        }
+        else {
+            return "Playing";
+        }
+    }
+
+    public Double getHowMuchFood()
+    {
+        return howMuchFood;
+    }
+
+    public void goToTheToilet()
+    {
+        weight = weight - 5;
+        System.out.println("pee");
+    }
+
+    public static Integer getCount()
+    {
+        return count;
+    }
+
+    public static void tvix(Cat cat1, Cat cat2)
+    {
+        cat1.weight=cat2.weight;
+        cat1.originWeight=cat2.originWeight;
+    }
+}
---
+++ src/Loader.java
@@ -0,0 +1,90 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Loader
+{
+    public static void main(String[] args) throws IOException
+    {
+
+        try
+        {
+            System.out.println(Cat.getCount());
+            Cat cat1 = new Cat(2000.0);
+            System.out.println(Cat.getCount());
+            cat1.meow();
+            System.out.println(cat1.getWeight());
+            System.out.println(Cat.getCount() + " but the cat is " + cat1.getStatus());
+            cat1.feed(15000.0);
+            System.out.println(Cat.getCount() + " but the cat is " + cat1.getStatus());
+
+//        //создаём котов, для третьего кота значение вема получаем из консоли
+//        Cat cat1 = new Cat();
+//        Cat cat2 = new Cat(1200.0);
+//        BufferedReader reader = new BufferedReader( new InputStreamReader(System.in));
+//        System.out.println("Enter the weight of the cat: ");
+//        Cat cat3 = new Cat(Double.parseDouble(reader.readLine().trim()));
+//        Cat cat4 = new Cat();
+//        Cat cat5 = new Cat();
+//        int howManyMeow=0;
+//
+//        System.out.println(cat1.getWeight());
+//        System.out.println(cat2.getWeight());
+//        System.out.println(cat3.getWeight());
+//        System.out.println(cat4.getWeight());
+//        System.out.println(cat5.getWeight());
+//        System.out.println(cat1.getStatus());
+//        System.out.println(cat2.getStatus());
+//        System.out.println(cat3.getStatus());
+//        System.out.println(cat4.getStatus());
+//        System.out.println(cat5.getStatus());
+//
+//        //выводим в консоль количество живых котов
+//        System.out.println("Live cats: " + Cat.getCount());
+//
+//        //делаем кота cat1 дубликатом кота cat2
+//        Cat.tvix(cat1, cat2);
+//        System.out.println(cat1.getWeight());
+//        System.out.println(cat2.getWeight());
+//        cat1.meow();
+//        System.out.println(cat1.getWeight());
+//        System.out.println(cat2.getWeight());
+//
+//        //кормим котов и проверяем их вес и вес съеденной ими еду
+//        cat1.feed(1000.0);
+//        System.out.println(cat1.getWeight());
+//        System.out.println(cat1.getHowMuchFood());
+//        cat1.feed(cat2.getWeight());
+//        System.out.println(cat1.getWeight());
+//        System.out.println(cat1.getHowMuchFood());
+//
+//        //отпровляем кота в туалет и после проверяем его вес
+//        cat2.goToTheToilet();
+//        System.out.println(cat2.getWeight());
+//
+//        //заставляем кота мяукать пока он не умрёт
+//        while (!cat1.getStatus().equals("Dead"))
+//        {
+//         cat1.meow();
+//         howManyMeow++;
+//        }
+//        System.out.println(howManyMeow);
+//
+//        //убиваем несколько котов и проверяем, то что количество живых котов становится меньше
+//        System.out.println("Live cats: " + Cat.getCount());
+//        cat5.feed(-1000000.0);
+//        System.out.println(cat5.getStatus());
+//        System.out.println(cat5.getStatus());
+//        System.out.println(cat5.getStatus());
+//        System.out.println("Live cats: " + Cat.getCount());
+//        cat4.feed(-1000000.0);
+//        System.out.println(cat4.getStatus());
+//        System.out.println(cat4.getStatus());
+//        System.out.println(cat4.getStatus());
+//        System.out.println("Live cats: " + Cat.getCount());
+
+        } catch (NumberFormatException e) {
+            System.err.println("Неверный формат строки!");
+        }
+    }
+}
